dcdc1597e67c26800d859e6636ce4d04b329b796
Furkan Dalyan <dmf.furkan@gmail.com>
2025-11-17 15:45:10 +0300

    Ders modeli eklendi ve rollerle ilişkilendirildi

--- eys/admin.py
+++ eys/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Role, User
+from .models import Role, User,Course
 
 admin.site.register(Role)
 admin.site.register(User)
+admin.site.register(Course)
--- eys/models.py
+++ eys/models.py
@@ -13,3 +13,30 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True)
+
+    instructor = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="courses_given",
+        limit_choices_to={'role__name__in': [
+            'Regular Instructor',
+            'Advisor Instructor',
+            'Head of Department',
+        ]}
+    )
+
+    students = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name="courses_taken",
+        limit_choices_to={'role__name': 'Student'}
+    )
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
